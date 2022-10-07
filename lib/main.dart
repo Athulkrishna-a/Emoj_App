4360291b6037d4057e774c379b11d74b772fbf99
Athulkrishna-a <-->
2022-10-07 11:55:28 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:emoj_app/splashscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+int? isViwed;
+void main() async {
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.greenAccent,
     ),
   );
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  isViwed = prefs.getInt("HomeView");
   runApp(
     const MyApp(),
   );
